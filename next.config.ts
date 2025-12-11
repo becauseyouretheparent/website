18a73f320470c73b3ead84ec3de4f11da7f2b974
The John <jsqueet08@gmail.com>
2025-12-11 17:30:52 -0500

    fix: forgot to remove a few lines.

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "https://www.becauseyouretheparent.com",
-        permanent: true, // uses 308 under the hood
-      },
-    ];
-  },
 };
 
 export default nextConfig;
