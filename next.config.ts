7c4abc172323629aad07d7bc69502bc5a7a822c7
The John <jsqueet08@gmail.com>
2025-12-11 17:28:39 -0500

    fix: Previous commit was causing an endless redirect loop.

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
-const isVercel = process.env.VERCEL === "1";
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
   async redirects() {
-
-    if (!isVercel) {
-      // Local dev -> no redirect
-      return [];
-    }
-
     return [
       {
         source: "/",
