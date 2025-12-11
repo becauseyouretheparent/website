e4b9c004d3847bb850d37c35d4f50cee5c8a1d42
The John <jsqueet08@gmail.com>
2025-12-11 17:19:23 -0500

    fix: only redirect apex domain in production.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,26 @@
 import type { NextConfig } from "next";
 
+const isVercel = process.env.VERCEL === "1";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  async redirects() {
+
+    if (!isVercel) {
+      // Local dev -> no redirect
+      return [];
+    }
+
+    return [
+      {
+        source: "/",
+        destination: "https://www.becauseyouretheparent.com",
+        permanent: true, // uses 308 under the hood
+      },
+    ];
+  },
 };
 
 export default nextConfig;
